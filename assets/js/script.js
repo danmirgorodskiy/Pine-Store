648928e72f334aa4bdbd9859b12289014649d80f
Daniel Mirgorodskiy <mirgorodskiy11@gmail.com>
2024-12-29 00:35:18 +0200

    Fixed navbar for mobile devices, added html layout for authentication

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
         const toast = new bootstrap.Toast(toastEl);
         toast.show();
     }
+
+    // Authentication
+    document.querySelector('#signup form').addEventListener('submit', function (e) {
+        const password = document.getElementById('signupPassword').value;
+        const confirmPassword = document.getElementById('confirmPassword').value;
+
+        if (password !== confirmPassword) {
+            e.preventDefault();
+            alert('Passwords do not match!');
+        }
+    });
+
+    function openTab(tabName) {
+        const triggerTab = document.querySelector(`[data-bs-target="#${tabName}"]`);
+        const tabInstance = new bootstrap.Tab(triggerTab);
+        tabInstance.show(); // Відкриває відповідну вкладку
+    }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const tabToOpen = urlParams.get('tab');
+    if (tabToOpen) {
+        openTab(tabToOpen);
+    }
 });
